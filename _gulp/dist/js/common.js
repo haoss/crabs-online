cd1aac90e76a7e92c3a4b0e1b0d1f32d19dc3c08
Dmitriy Khristoev <haossut@gmail.com>
2019-11-04 17:23:51 +0200

    new styles

---
+++ _gulp/dist/js/common.js
@@ -0,0 +1,90 @@
+'use strict';
+
+// Document ready
+$(document).on('ready', function(){
+
+  var scroll = new SmoothScroll('a[data-scroll]', {
+    speed: 800,
+    updateURL: false
+  });
+
+  // Magnific popup gallery
+  $('.gallery').each(function() {
+    $(this).magnificPopup({
+      delegate: '.gallery-item',
+      type: 'image',
+      gallery:{
+        enabled:true
+      },
+      zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+          // openerElement is the element on which popup was initialized, in this case its <a> tag
+          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+          return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
+  });
+
+  // Magnific popup one image
+  $('.image-popup').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    mainClass: 'mfp-img-mobile',
+    image: {
+      verticalFit: true
+    }
+  });
+
+  // Magnific popup video
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+
+  $('.open-popup-link').magnificPopup({
+    type: 'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });
+
+  navigation();
+});
+
+$(window).on('load', function() {
+  $(".loader").delay(400).fadeOut("slow");
+});
+
+$(window).on('scroll', function() { });
+$(window).on('resize', function() { });
+
+function navigation() {
+  var btn = $('.header__btn');
+
+  btn.on('click', function() {
+    var _this = $(this);
+    var body = $('body');
+    var nav = $('.navigation');
+    
+    if (_this.hasClass('is-active')) {
+      _this.removeClass('is-active');
+      body.removeClass('is-active');
+      nav.removeClass('is-active');
+    } else {
+      _this.addClass('is-active');
+      body.addClass('is-active');
+      nav.addClass('is-active');
+    }
+  })
+}
