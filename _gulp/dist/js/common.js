0c582535e29f77c825332c1ef53c58460b508b0e
Dmitriy Khristoev <haossut@gmail.com>
2019-11-14 17:04:27 +0200

    new styles

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -114,6 +114,7 @@ $(document).on('ready', function(){
 
   navigation();
   scrollPage();
+  btnUp();
 
   //Chrome Smooth Scroll
   try {
@@ -130,7 +131,9 @@ $(window).on('load', function() {
   $(".loader").delay(400).fadeOut("slow");
 });
 
-$(window).on('scroll', function() { });
+$(window).on('scroll', function() {
+  btnUp();
+});
 $(window).on('resize', function() { });
 
 function navigation() {
@@ -157,8 +160,23 @@ function scrollPage() {
   var controller1 = new ScrollMagic.Controller();
   var controller2 = new ScrollMagic.Controller();
   
-  // Scene 1
   var scene1Tl = new TimelineMax();
+  var scene2Tl = new TimelineMax();
+  var scene3Tl = new TimelineMax();
+  var scene4Tl = new TimelineMax();
+  var scene5Tl = new TimelineMax();
+  var scene6Tl = new TimelineMax();
+  var scene7Tl = new TimelineMax();
+  var scene8Tl = new TimelineMax();
+  
+  var btn1Tl = new TimelineMax();
+  var btn2Tl = new TimelineMax();
+  var btn3Tl = new TimelineMax();
+  var btn4Tl = new TimelineMax();
+  var btn5Tl = new TimelineMax();
+  var btn6Tl = new TimelineMax();
+
+  // Scene 1
   scene1Tl
     .fromTo('.home__title', 1.5, {autoAlpha: 0, y: 50}, {autoAlpha: 1, y: 0}, 'scene1Tl-line1')
     .fromTo('.section__text--one', 1.5, {y: '-=15px'}, {y: 0}, 'scene1Tl-line1')
@@ -175,9 +193,12 @@ function scrollPage() {
     .addTo(controller1);
 
   // Btn scrollTo 1
-  var btn1Tl = new TimelineMax();
   btn1Tl
-    .fromTo('#section-btn1', 1, {autoAlpha: 0, y: 25}, {autoAlpha: 1, y: 0});
+    .fromTo('#section-btn1', 1, {autoAlpha: 0, y: 25}, {autoAlpha: 1, y: 0})
+    .fromTo('#section-btn1 > span', 0.5, {autoAlpha: 0, height: 0}, {autoAlpha: 1, height: '160px'})
+    .fromTo('#section-btn1 > span > span', 0.5, {autoAlpha: 0}, {autoAlpha: 1})
+    .fromTo('#section-btn1 > span > span > span', 0.5, {autoAlpha: 0}, {autoAlpha: 1})
+  ;  
   var btnScene1 = new ScrollMagic.Scene({
     triggerElement: "#section2",
     triggerHook: 0.9
@@ -186,11 +207,10 @@ function scrollPage() {
     .addTo(controller2);
 
   // Scene 2
-  var scene2Tl = new TimelineMax();
   scene2Tl
     .fromTo('#about__container', 1.5, {autoAlpha: 0, y: 100}, {autoAlpha: 1, y: 0})
   ;
-  var scene2= new ScrollMagic.Scene({
+  var scene2 = new ScrollMagic.Scene({
     triggerElement: "#section2",
     triggerHook: 0.6,
     reverse: false
@@ -199,7 +219,6 @@ function scrollPage() {
     .addTo(controller1);
 
   // Btn scrollTo 2
-  var btn2Tl = new TimelineMax();
   btn2Tl
     .fromTo('#section-btn2', 1, {autoAlpha: 0, y: 25}, {autoAlpha: 1, y: 0});
   var btnScene2 = new ScrollMagic.Scene({
@@ -209,20 +228,8 @@ function scrollPage() {
     .setTween(btn2Tl)
     .addTo(controller2);
 
-  // Body up
-  var btnUpTl = new TimelineMax();
-  btnUpTl
-    .to('.body__up', 0.5, {className:"+=is-visible"})
-  ;
-  var btnUpScene = new ScrollMagic.Scene({
-    triggerElement: "#section2",
-    triggerHook: 0.2
-  })
-    .setTween(btnUpTl)
-    .addTo(controller2);
-
   // Scene 3
-  var scene3Tl = new TimelineMax();
+  
   scene3Tl
     .fromTo('#section3 .section__header', 1.5, {autoAlpha: 0, y: 25}, {autoAlpha: 1, y: 0})
     .staggerFromTo( '.services__block', 0.5, {autoAlpha: 0, y: 25}, {autoAlpha: 1, y: 0}, 0.25)
@@ -237,7 +244,7 @@ function scrollPage() {
     .addTo(controller1);
 
   // Btn scrollTo 3
-  var btn3Tl = new TimelineMax();
+  
   btn3Tl
     .fromTo('#section-btn3', 1, {autoAlpha: 0, y: 25}, {autoAlpha: 1, y: 0});
   var btnScene3 = new ScrollMagic.Scene({
@@ -248,7 +255,6 @@ function scrollPage() {
     .addTo(controller2);
 
   // Scene 4
-  var scene4Tl = new TimelineMax();
   scene4Tl
     .fromTo('#section4 .section__header', 1, {autoAlpha: 0, x: '-=50px'}, {autoAlpha: 1, x: 0})
     .fromTo('.projects__nav', 1, {autoAlpha: 0, x: 50}, {autoAlpha: 1, x: 0})
@@ -263,7 +269,6 @@ function scrollPage() {
     .addTo(controller1);
 
   // Btn scrollTo 4
-  var btn4Tl = new TimelineMax();
   btn4Tl
     .fromTo('#section-btn4', 1, {autoAlpha: 0, y: 25}, {autoAlpha: 1, y: 0});
   var btnScene4 = new ScrollMagic.Scene({
@@ -274,7 +279,6 @@ function scrollPage() {
     .addTo(controller2);
 
   // Scene 5
-  var scene5Tl = new TimelineMax();
   scene5Tl
     .fromTo('#section5 .section__header', 1.5, {autoAlpha: 0, x: '-=50px'}, {autoAlpha: 1, x: 0})
     .staggerFromTo( '.achievements__block', 1, {autoAlpha: 0, y: 50}, {autoAlpha: 1, y: 0}, 0.25)
@@ -288,7 +292,6 @@ function scrollPage() {
     .addTo(controller1);
 
   // Btn scrollTo 5
-  var btn5Tl = new TimelineMax();
   btn5Tl
     .fromTo('#section-btn5', 1, {autoAlpha: 0, y: 25}, {autoAlpha: 1, y: 0});
   var btnScene5 = new ScrollMagic.Scene({
@@ -299,7 +302,6 @@ function scrollPage() {
     .addTo(controller2);
 
   // Scene 6
-  var scene6Tl = new TimelineMax();
   scene6Tl
     .fromTo('#section6 .section__header', 1.5, {autoAlpha: 0, x: '-=50px'}, {autoAlpha: 1, x: 0})
     .staggerFromTo( '.clients__block', 1, {autoAlpha: 0, y: 50}, {autoAlpha: 1, y: 0}, 0.25)
@@ -313,9 +315,12 @@ function scrollPage() {
     .addTo(controller1);
 
   // Btn scrollTo 6
-  var btn6Tl = new TimelineMax();
   btn6Tl
-    .fromTo('#section-btn6', 1, {autoAlpha: 0, y: 25}, {autoAlpha: 1, y: '-=75px'});
+    .fromTo('#section-btn6', 1, {autoAlpha: 0, y: 25}, {autoAlpha: 1, y: 0})
+    .fromTo('#section-btn6 > span', 0.5, {autoAlpha: 0, height: 0}, {autoAlpha: 1, height: '160px'})
+    .fromTo('#section-btn6 > span > span', 0.5, {autoAlpha: 0}, {autoAlpha: 1})
+    .fromTo('#section-btn6 > span > span > span', 0.5, {autoAlpha: 0}, {autoAlpha: 1})
+  ;
   var btnScene6 = new ScrollMagic.Scene({
     triggerElement: "#section7",
     triggerHook: 0.9
@@ -324,7 +329,6 @@ function scrollPage() {
     .addTo(controller2);
 
   // Scene 7
-  var scene7Tl = new TimelineMax();
   scene7Tl
     .fromTo('.request__block', 1.5, {autoAlpha: 0, x: 50}, {autoAlpha: 1, x: 0})
   ;
@@ -337,7 +341,6 @@ function scrollPage() {
     .addTo(controller1);
 
   // Scene 8
-  var scene8Tl = new TimelineMax();
   scene8Tl
     .fromTo('.seo', 1.5, {autoAlpha: 0, y: 50}, {autoAlpha: 1, y: 0})
     .fromTo('.footer__center', 1.5, {autoAlpha: 0, y: 50}, {autoAlpha: 1, y: 0})
@@ -356,5 +359,18 @@ function scrollPage() {
   if (width < 991) {
     controller1.destroy(true);
     TweenLite.set('*', {clearProps:"all"});
+  }
+}
+
+function btnUp() {
+  var width = $(window).width();
+  var btn = $('.body__up');
+
+  if (btn.length <= 0) return;
+
+  if ($(window).scrollTop() >= 500) {
+    btn.addClass('is-visible');
+  } else {
+    btn.removeClass('is-visible');
   }
 }
